c829eb2af94b8cc9e874fc4d2733a00ac8909484
isuretpolos <isuret.polos@gmx.net>
2019-07-11 09:00:43 +0200

    added few components and routing works fine

--- gui/src/app/app-routing.module.ts
+++ gui/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {AnalysisComponent} from "./components/analysis/analysis.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'analysis', component: AnalysisComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AnalysisComponent } from './components/analysis/analysis.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AnalysisComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     NgbModule.forRoot()
   ],
   providers: [],
