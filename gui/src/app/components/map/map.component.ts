7aac0b5003024ac221f7ebb20895d49e90e64a7a
isuretpolos <isuret.polos@gmx.net>
2019-10-20 16:07:23 +0200

    Colorize grid WIP ... one missing

--- gui/src/app/components/map/map.component.ts
+++ gui/src/app/components/map/map.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MapObject} from "../../map/map";
 import {AreaService} from "../../services/area.service";
 import * as olStyle from 'ol/style';
 import {AnalysisResult, AreaAnalysis} from "../../domain/analysisResult";
 import {AetherServerService} from "../../services/aether-server.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-map',
@@ -13,16 +14,26 @@ import {AetherServerService} from "../../services/aether-server.service";
 export class MapComponent implements OnInit {
 
   map: MapObject;
-  scanning:boolean = false;
-  areaAnalysis:AreaAnalysis;
-  gridResult:AnalysisResult;
+  scanning: boolean = false;
+  areaAnalysis: AreaAnalysis;
+  gridResult: AnalysisResult;
+  rateNames: string[] = [];
+  public analysisSettingsForm: FormGroup;
+  selectedRateName:string;
 
   constructor(
     private areaService: AreaService,
+    private formbuilder: FormBuilder,
     private aetherServerService: AetherServerService
-    ) { }
+  ) {
+  }
 
   ngOnInit() {
+
+    this.analysisSettingsForm = this.formbuilder.group({
+      rateNames: ''
+    });
+
     this.map = new MapObject(this.areaService);
     this.map.init();
     let map = this.map.map;
@@ -31,6 +42,13 @@ export class MapComponent implements OnInit {
       map.forEachFeatureAtPixel(map.getEventPixel(e), (feature, layer) => {
         this.gridResult = feature.analysisResult;
       });
+    });
+
+    this.aetherServerService.getAllRateNames().subscribe(rateNames => {
+      for (let i = 0, len = rateNames.length; i < len; i++) {
+        let rate = rateNames[i] as string;
+        this.rateNames.push(rate);
+      }
     });
   }
 
@@ -49,15 +67,17 @@ export class MapComponent implements OnInit {
   }
 
   scanArea() {
-    console.log(this.map.lastSketch);
+    if (this.analysisSettingsForm.getRawValue().rateNames.length < 3) return;
+
+    this.selectedRateName = this.analysisSettingsForm.getRawValue().rateNames;
     this.scanning = true;
     this.areaAnalysis = new AreaAnalysis();
 
-    if (this.map.lastSketch) {
+    if (this.map.lastSketch != null) {
       this.map.source.removeFeature(this.map.lastSketch);
     }
 
-    this.aetherServerService.checkGeneralVitality().subscribe( gv => {
+    this.aetherServerService.checkGeneralVitality().subscribe(gv => {
       this.areaAnalysis.generalVitality = gv;
       this.scanGrid();
     });
@@ -66,7 +86,7 @@ export class MapComponent implements OnInit {
   private scanGrid() {
     if (this.areaAnalysis == null) return;
 
-    this.aetherServerService.analyzeAreaGrid("HOMEOPATHY_Clarke_With_MateriaMedicaUrls.txt").subscribe( analysisResult => {
+    this.aetherServerService.analyzeAreaGrid(this.selectedRateName).subscribe(analysisResult => {
 
       console.log(analysisResult);
 
@@ -79,7 +99,7 @@ export class MapComponent implements OnInit {
       let gridNumber = this.areaAnalysis.gridAnalysis.length;
       this.colorGrid(gridNumber);
 
-      if (gridNumber >= 64) {
+      if (gridNumber > 68) {
         this.scanning = false;
       } else {
         this.scanGrid();
@@ -87,14 +107,28 @@ export class MapComponent implements OnInit {
     });
   }
 
-  private colorGrid(gridNumber:number) {
-
-    let analysisResult:AnalysisResult = this.areaAnalysis.gridAnalysis[gridNumber - 1];
+  private colorGrid(gridNumber: number) {
+console.log(gridNumber);
+    let analysisResult: AnalysisResult = this.areaAnalysis.gridAnalysis[gridNumber - 1];
     let counter = 0;
-    let r = this.randomInt(0, 255);
-    let g = this.randomInt(0, 255);
-    let b = this.randomInt(0, 255);
-    let alpha = this.randomInt(1, 50);
+
+    let allGV = this.areaAnalysis.generalVitality;
+    let gridGV = analysisResult.generalVitality;
+
+    let r = 255;
+    let g = 255;
+    let b = 255;
+    let alpha = 50;
+
+    if (gridGV > allGV) {
+      r = 0; g = 255; b = 0;
+      alpha = gridGV - allGV;
+    } else if (gridGV < allGV) {
+      r = 255; g = 0; b = 0;
+      alpha = allGV - gridGV;
+    } else {
+      r = 0; g = 0; b = 255;
+    }
 
     for (let feature of this.map.source.getFeatures()) {
       if (counter == gridNumber) {
@@ -116,7 +150,11 @@ export class MapComponent implements OnInit {
     }
   }
 
-  private randomInt(min, max){
+  private randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  selectRateName() {
+
   }
 }
