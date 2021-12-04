353e8e573732c4a1f9b037d2f839068fa1d9fe44
isuretpolos <isuret.polos@gmx.net>
2021-12-04 18:02:59 +0100

    VERSION 1.2.4

--- startGui.bat
+++ startGui.bat
@@ -1,2 +1,2 @@
-java -jar AetherOnePi-1.2.3-jar-with-dependencies.jar
+java -jar AetherOnePi-1.2.4-jar-with-dependencies.jar
 rem pause
--- startServer.bat
+++ startServer.bat
@@ -1,2 +1,2 @@
-java -jar AetherOnePi-1.2.3.jar
+java -jar AetherOnePi-1.2.4.jar
 pause
